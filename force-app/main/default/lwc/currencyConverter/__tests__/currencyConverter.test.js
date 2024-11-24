6f9be5dba7b3bad284abe7fbc98b9ca18fc4d718
Asheesh25 <asheesh.upadhyay2512@gmail.com>
2024-11-24 18:53:57 +0530

    Code on 24th Nov

--- force-app/main/default/lwc/currencyConverter/__tests__/currencyConverter.test.js
+++ force-app/main/default/lwc/currencyConverter/__tests__/currencyConverter.test.js
@@ -1,4 +1,4 @@
-import { createElement } from 'lwc';
+import { createElement } from '@lwc';
 import CurrencyConverter from 'c/currencyConverter';
 
 describe('c-currency-converter', () => {
--- force-app/main/default/lwc/currencyConverter/currencyConverter.js
+++ force-app/main/default/lwc/currencyConverter/currencyConverter.js
@@ -1,3 +1,21 @@
 import { LightningElement } from 'lwc';
+import {countryCodeList} from 'c/countryCodeList';
+// import bgImage from '@salesforce/resourceUrl/currencyBackground';
+export default class CurrencyConverter extends LightningElement {
+    codeList = countryCodeList;
+    countryFrom = 'USD';
+    countryTo = 'AUD';
+    // bgImg = bgImage;
 
-export default class CurrencyConverter extends LightningElement {}
+    connectedCallback(){
+        console.log('Country code list is ', this.codeList);
+        console.log('Background image is ', this.bgImg);
+    }
+
+    handleChange(event){
+        const {name,value} = event.target;
+        this[name] = value;
+        // console.log('Value is ',value,'and country from is ',this.countryFrom);
+        // console.log('Value is ',value,' and country to is ',this.countryTo);
+    }
+}
